19b1116e3530ebc78e721c65c0777567d5210086
Michael Kravitz <30937928+Xthe23@users.noreply.github.com>
2020-11-06 09:38:22 -0800

    Modified gitignore and added images to images folder

---
+++ src/data.js
@@ -0,0 +1,106 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name:'Black Supreme Beanie',
+      category: "Beanies",
+      image: "/images/p1.jpg",
+      price: 180,
+      Brand: "Supreme",
+      rating: 5,
+      numReviews: 10,
+      description: 'A Beanie to keep your head warm'
+    },
+    {
+      _id: '2',
+      name:'Grey Supreme Beanie',
+      category: "Beanies",
+      image: "/images/p2.jpg",
+      price: 180,
+      Brand: "Supreme",
+      rating: 4.5,
+      numReviews: 10,
+      description: 'A Beanie to keep your head warm'
+    },
+    {
+      _id: '3',
+      name:'Pink Supreme Beanie',
+      category: "Beanies",
+      image: "/images/p3.jpg",
+      price: 180,
+      Brand: "Supreme",
+      rating: 5,
+      numReviews: 10,
+      description: 'A Beanie to keep your head warm'
+    },
+    {
+      _id: '4',
+      name:'Red Supreme Beanie',
+      category: "Beanies",
+      image: "/images/p4.jpg",
+      price: 180,
+      Brand: "Supreme",
+      rating: 4,
+      numReviews: 10,
+      description: 'A Beanie to keep your head warm'
+    },
+    {
+      _id: '5',
+      name:'Cloud Supreme Beanie',
+      category: "Beanies",
+      image: "/images/p5.jpg",
+      price: 180,
+      Brand: "Supreme",
+      rating: 4.5,
+      numReviews: 10,
+      description: 'A Beanie to keep your head warm'
+    },
+    {
+      _id: '6',
+      name:'Brown Supreme Beanie',
+      category: "Beanies",
+      image: "/images/p6.jpg",
+      price: 180,
+      Brand: "Supreme",
+      rating: 5,
+      numReviews: 10,
+      description: 'A Beanie to keep your head warm'
+    },
+    {
+      _id: '7',
+      name:'Dreamcube Longsleeve Shirt',
+      category: "Longsleeve Shirts",
+      image: "/images/p7.png",
+      price: 70,
+      Brand: "Vapor95",
+      rating: 5,
+      numReviews: 10,
+      description: 'Reminds us all of the Gamecube Days'
+    },
+    {
+      _id: '8',
+      name:'AOL Bomber Jacket',
+      category: "Jackets",
+      image: "/images/p8.png",
+      price: 180,
+      Brand: "AOL",
+      rating: 5,
+      numReviews: 10,
+      description: "The 90's was a trying time for many but this jacket helped"
+    },
+    {
+      _id: '9',
+      name:'MySpace Bomber Jacket',
+      category: "Jackets",
+      image: "/images/p9.png",
+      price: 180,
+      Brand: "MySpace",
+      rating: 5,
+      numReviews: 10,
+      description: "The 90's was a trying time for many but this jacket helped"
+    },
+
+  ]
+}
+
+export default data;
